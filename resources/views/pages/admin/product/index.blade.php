2243a53b5a3fb0d4380b6cd0c9a2130d1866940a
H P <nurchumairohahmad@gmail.com>
2024-10-09 21:27:22 +0700

    CRUD

--- resources/views/pages/admin/product/index.blade.php
+++ resources/views/pages/admin/product/index.blade.php
@@ -6,14 +6,13 @@
         <div class="section-header">
             <h1>Produk</h1>
             <div class="section-header-breadcrumb">
-                <div class="breadcrumb-item active">
-                    <a href="{{ route('admin.dashboard') }}">Dashboard</a>
-                </div>
+                <div class="breadcrumb-item active"><a href="{{ route('admin.dashboard') }}">
+                        Dashboard</a></div>
                 <div class="breadcrumb-item">Produk</div>
             </div>
         </div>
-        <a href="{{ route('product.create') }}" class="btn btn-icon icon-left btnprimary"><i class="fas fa-plus"></i>
-            Produk</a>
+        <a href="{{ route('product.create') }}" class="btn btn-icon icon-left btn-primary">
+            <i class="fas fa-plus"></i> Produk</a>
         <div class="card-body">
             <div class="table-responsive">
                 <table class="table table-bordered table-md">
@@ -32,9 +31,10 @@
                             <td>{{ $item->name }}</td>
                             <td>{{ $item->price }} Points</td>
                             <td>
-                                <a href="#" class="badge badge-info">Detail</a>
-                                <a href="#" class="badge badge-warning">Edit</a>
-                                <a href="" class="badge badge-danger">Hapus</a>
+                                <a href="{{ route('product.detail', $item->id) }}" class="badge badge-info">Detail</a>
+                                <a href="{{ route('product.edit', $item->id) }}" class="badge badge-warning">Edit</a>
+                                <a href="{{ route('product.delete', $item->id) }}" class="badge badge-danger"
+                                    data-confirm-delete="true">Hapus</a>
                             </td>
                         </tr>
                     @empty
